f854c393aa07c234773a0d925576adbe338932c1
Jack Cooper <jack.cooper7776@gmail.com>
2022-10-19 17:16:49 +1100

    Removed whitespace

--- main.cs
+++ main.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-
 class Program {
     public static void Main(string[] args) {
         Quote q = GetQuoteAsync().Result;
